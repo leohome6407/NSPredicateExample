c0ba9e4ba9ef27afe6c8dd22f638d1f3d5d2fbcb
leo.lee <leo.lee@mtaxi.com.tw>
2019-08-07 11:14:37 +0800

    正則表達式第一次上傳。

---
+++ NSPredicateExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NSPredicateExample
+//
+//  Created by mtaxi on 2019/8/7.
+//  Copyright © 2019 leo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NSPredicateExample/ViewController.m
@@ -0,0 +1,73 @@
+//
+//  ViewController.m
+//  NSPredicateExample
+//
+//  Created by mtaxi on 2019/8/7.
+//  Copyright © 2019 leo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (NSInteger)getPriceFromString:(NSString *)price
+{
+	//正則表達式過濾條件
+	NSString *pattern = @"[0-9]{1,9}"; //範圍只能0到9的數字，最少1一個，最多有9個字元
+	
+	NSRegularExpression *reg = [NSRegularExpression regularExpressionWithPattern:pattern options:NSRegularExpressionDotMatchesLineSeparators error:NULL];
+	NSRange firstRange = [reg rangeOfFirstMatchInString:price options:NSMatchingReportProgress range:NSMakeRange(0, price.length)];
+	
+	NSString *filterResult = [price substringWithRange:firstRange];
+	
+	NSLog(@"匹配到第一組的位置:開始位置%lu--長度%lu",(unsigned long)firstRange.location,(unsigned long)firstRange.length);
+	NSLog(@"過濾結果為 %@",filterResult);
+	return [filterResult integerValue];
+}
+
+- (NSInteger)getPriceFromString1:(NSString *)price
+{
+	//正則表達式過濾條件
+	NSString *pattern = @"~+[0-9]{1,9}";
+	
+	NSRegularExpression *reg = [NSRegularExpression regularExpressionWithPattern:pattern options:NSRegularExpressionDotMatchesLineSeparators error:NULL];
+	
+	NSRange firstRange = [reg rangeOfFirstMatchInString:price options:NSMatchingReportProgress range:NSMakeRange(0, price.length)];
+	
+	NSString *filterResult = [price substringWithRange:firstRange];
+	
+	filterResult = [filterResult stringByReplacingOccurrencesOfString:@"~" withString:@""];
+	
+	NSLog(@"匹配到第一組的位置:開始位置%lu--長度%lu",(unsigned long)firstRange.location,(unsigned long)firstRange.length);
+	NSLog(@"過濾結果為 %@",filterResult);
+	return [filterResult integerValue];
+}
+
+//無~符號的字串驗證
+-(BOOL)verifyPrice:(NSString *)price{
+	NSString *fareRegex = @"[\u4e00-\u9fa5]*[0-9]{1,9}[\u4e00-\u9fa5]*";
+	NSPredicate *test = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", fareRegex];
+	BOOL matches = [test evaluateWithObject:price];
+	NSLog(matches ? @"有預估車資" : @"沒有預估車資");
+	return matches;
+}
+
+//有~符號的字串驗證
+-(BOOL)verifyPrice1:(NSString *)price{
+	NSString *fareRegex = @"預估+.+元";
+	NSPredicate *test = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", fareRegex];
+	BOOL matches = [test evaluateWithObject:price];
+	NSLog(matches ? @"有預估車資" : @"沒有預估車資");
+	return matches;
+}
+
+@end
